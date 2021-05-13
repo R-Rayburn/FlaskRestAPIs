485efcf460f434865a8325fda535cedc9ef379bd
Robert Rayburn <robert.rayburn@core10.io>
2021-05-13 11:19:50 -0500

    example of applying decorators with at symbol

---
+++ python_refresher/at_syntax_for_decorators.py
@@ -0,0 +1,26 @@
+import functools
+
+
+user = {"username": "jose", "access_level": "guest"}
+
+
+# Decorator
+def make_secure(func):
+    # Need this on any custom decorators
+    @functools.wraps(func)
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+        else:
+            return f"No admin permissions for {user['username']}"
+    return secure_function
+
+
+@make_secure
+def get_admin_password():
+    return "1234"
+
+
+print(get_admin_password())
+get_admin_password = make_secure(get_admin_password)
+print(get_admin_password.__name__)
