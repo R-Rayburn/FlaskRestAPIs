eebc551e2744f5636fbcfa4eb046c98a9d904e6f
Robert Rayburn <robert.rayburn@core10.io>
2021-05-13 11:19:14 -0500

    example of decorating a function with parameters

---
+++ python_refresher/decorating_functions_with_parameters.py
@@ -0,0 +1,27 @@
+import functools
+
+
+user = {"username": "jose", "access_level": "admin"}
+
+
+# Decorator
+def make_secure(func):
+    # Need this on any custom decorators
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if user["access_level"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            return f"No admin permissions for {user['username']}"
+    return secure_function
+
+
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
+
+
+print(get_password("billing"))
