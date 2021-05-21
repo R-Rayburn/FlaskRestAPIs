b2a363de734e4a49593d7f8f8cd832cf4f4e9832
Robert Rayburn <robert.rayburn@core10.io>
2021-05-20 22:43:09 -0500

    added ItemList GET and updated Item POST to pull data from API body

--- venv_code/app.py
+++ venv_code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -14,13 +14,23 @@ class Item(Resource):
         return {'item': None, 'message': f'{name} was not found'}, 404
 
     def post(self, name):
+        # get_json params:
+        # force -> True means content header doesn't need to be specified
+        # silent -> True means it will give you none instead of error
+        request_data = request.get_json()
         if name in [item['name'] for item in items]:
             return {'message': f'{name} already exists'}, 404
-        item = {'name': name, 'price': 10.00}
+        item = {'name': name, 'price': request_data['price']}
         items.append(item)
         return item, 201
 
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5005)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5005, debug=True)
