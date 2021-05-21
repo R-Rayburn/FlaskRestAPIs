5b52883a41f1951bccc9087f4723693e9248d48f
Robert Rayburn <robert.rayburn@core10.io>
2021-05-20 22:31:32 -0500

    implemented item get and post methods

--- venv_code/app.py
+++ venv_code/app.py
@@ -3,13 +3,24 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+items = []
 
 
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None, 'message': f'{name} was not found'}, 404
+
+    def post(self, name):
+        if name in [item['name'] for item in items]:
+            return {'message': f'{name} already exists'}, 404
+        item = {'name': name, 'price': 10.00}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5005)
