637017a06651fbd25ae3cff7d293f49345dc0f69
Robert Rayburn <robert.rayburn@core10.io>
2021-05-22 15:36:17 -0500

    registered UserRegister API

--- flask_application/section5/code/app.py
+++ flask_application/section5/code/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
+
 from security import authenticate, identity
+
+from user import UserRegister
 
 app = Flask(__name__)
 # This should be secure when using prod
@@ -14,7 +17,10 @@ items = []
 
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('price', type=float, required=True, help='This field cannot be blank.')
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='This field cannot be blank.')
 
     @jwt_required()
     def get(self, name):
@@ -22,10 +28,11 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        # get_json params:
-        # force -> True means content header doesn't need to be specified
-        # silent -> True means it will give you none instead of error
-        if next(filter(lambda i: i['name'] == name, items), None) is not None:
+        if next(
+                filter(
+                    lambda i: i['name'] == name,
+                    items),
+                None) is not None:
             return {'message': f'{name} already exists'}, 400
 
         data = Item.parser.parse_args()
@@ -58,5 +65,7 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5005, debug=True)
+
