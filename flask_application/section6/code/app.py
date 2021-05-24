cd0739a26e36078f6a41ec7b922b8623a9f1be87
Robert Rayburn <robert.rayburn@core10.io>
2021-05-24 17:02:09 -0500

    added code to create the tables and set up sql-alchemy

--- flask_application/section6/code/app.py
+++ flask_application/section6/code/app.py
@@ -8,9 +8,18 @@ from resources.user import UserRegister
 from security import authenticate, identity
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # This should be secure when using prod
 app.secret_key = 'Jose'
 api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<string:name>')
@@ -19,4 +28,6 @@ api.add_resource(UserRegister, '/register')
 
 # Only run if app.py is ran directly
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5005, debug=True)
