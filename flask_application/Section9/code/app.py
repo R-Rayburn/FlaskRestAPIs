18e08b8d1b42a4b917db992cd5c3f59d886d366d
Robert Rayburn <robert.rayburn@core10.io>
2021-05-26 13:19:13 -0500

    added user resource to get and delete

--- flask_application/Section9/code/app.py
+++ flask_application/Section9/code/app.py
@@ -3,7 +3,7 @@ from flask_jwt import JWT
 from flask_restful import Api
 
 from resources.item import Item, ItemList
-from resources.user import UserRegister
+from resources.user import User, UserRegister
 from resources.store import Store, StoreList
 
 from security import authenticate, identity
@@ -28,6 +28,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
+api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserRegister, '/register')
 
 # Only run if app.py is ran directly
--- flask_application/Section9/code/models/user.py
+++ flask_application/Section9/code/models/user.py
@@ -20,6 +20,13 @@ class UserModel(db.Model):
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
 
+    def json(self):
+        return {'id': self.id, 'username': self.username}
+
     def save_to_db(self):
         db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
         db.session.commit()
