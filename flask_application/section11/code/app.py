3911151c1f5d2ae1a227c19ebf0e186d14032ffc
Robert Rayburn <robert.rayburn@core10.io>
2021-05-29 22:09:46 -0500

    added token in block list loader to check if the token has been blacklisted

--- flask_application/section11/code/app.py
+++ flask_application/section11/code/app.py
@@ -5,11 +5,14 @@ from flask_restful import Api
 from resources.item import Item, ItemList
 from resources.user import User, UserRegister, UserLogin, TokenRefresh
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+# app.config['JWT_BLACKLIST_ENABLED'] = True
+# app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 # This should be secure when using prod
 app.secret_key = 'Jose'  # app.config['JWT_SECRET_KEY']
 api = Api(app)
@@ -28,6 +31,12 @@ def add_claims_to_jwt(identity):
     if identity == 1:  # value to check should be read from config or db.
         return {'is_admin': True}
     return {'is_admin': False}
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(header, payload):
+    print(payload['jti'])
+    return payload['jti'] in BLACKLIST
 
 
 @jwt.expired_token_loader
@@ -63,7 +72,7 @@ def needs_fresh_token_callback():
 
 
 @jwt.revoked_token_loader
-def revoked_token_callback():
+def revoked_token_callback(header, payload):
     return jsonify({
         'description': 'This token has been revoked.',
         'error': 'revoked_token'
